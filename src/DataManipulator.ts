28c1f90d72c0fecf72cb5297166faaab7bbefae8
AaryanPK <aaryanpickeringkumar@outlook.com>
2023-08-16 12:49:00 +0100

    Updated DataManipulator

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,5 +1,6 @@
 import { ServerRespond } from './DataStreamer';
 
+//Added each of the graph's attributes.
 export interface Row {
   timestamp: Date,
   abcPrice: number,
@@ -13,19 +14,32 @@ export interface Row {
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
+
+    //Gets the timestamp of the two stocks and chooses the larger one.
     let timestamp = serverResponds[0].timestamp;
     if (serverResponds[1].timestamp > serverResponds[0].timestamp) {
       timestamp = serverResponds[1].timestamp;
     }
+
+    //Calculates the prices of the two stocks using their top ask and top bid prices.
     const abc = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
     const def = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+
+    //Calculates the ratio of the two stocks.
     const ratio = abc / def;
-    const lb = 1 - 0.1 * ratio;
-    const ub = 1 + 0.1 * ratio;
+
+    //Calculates the upper and lower bounds (used +/- 5% instead of 10% to show the alert works).
+    const lb = 1 - 0.05 * ratio;
+    const ub = 1 + 0.05 * ratio;
+
+    //An alert appears (with the value of the ratio) if the ratio goes below the lower bound or above the upper bound, and is undefined 
+    //otherwise. 
     let alert;
     if ((ratio < lb) || (ratio > ub)) {
       alert = ratio;
     }
+
+    //Returns each of the data items.
     return {
       timestamp,
       abcPrice: abc,
