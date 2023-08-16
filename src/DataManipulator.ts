a9cbeef625007ecdcc923a053f9b94e456af09ea
AaryanPK <aaryanpickeringkumar@outlook.com>
2023-08-16 12:14:28 +0100

    Updated DataManipulator

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -13,20 +13,27 @@ export interface Row {
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
+    let timestamp = serverResponds[0].timestamp;
+    if (serverResponds[1].timestamp > serverResponds[0].timestamp) {
+      timestamp = serverResponds[1].timestamp;
+    }
     const abc = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
     const def = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = abc / def;
-    const lb = 1 - 0.05;
-    const ub = 1 + 0.05;
+    const lb = 1 - 0.1 * ratio;
+    const ub = 1 + 0.1 * ratio;
     let alert;
     if ((ratio < lb) || (ratio > ub)) {
       alert = ratio;
     }
     return {
-      timestamp: 
+      timestamp,
       abcPrice: abc,
       defPrice: def,
       ratio,
+      lowerBound: lb,
+      upperBound: ub,
+      alert,
     }
   }
 }
